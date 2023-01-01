a409f9b60cc545db64468fdb9e961552d7e1f3d1
xqq <15591388167@163.com>
2023-01-01 16:11:37 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<thread>
+#include<chrono>
+#include "threadpool.hpp"
+int main()
+{
+    ThreadPool pool;
+    pool.start(4);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    return 0;
+}
