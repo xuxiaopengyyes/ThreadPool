e218a6417e4bb8c6dcd6bcb20253cf46df380005
xqq <15591388167@163.com>
2023-02-19 21:23:04 +0800

    pool.h

--- Y1/ThreadPool.cpp
+++ Y1/ThreadPool.cpp
@@ -12,3 +12,31 @@
 
 const int KeepAliveTime = 10;
 template<class T>
+class SyncQueue
+{
+private:
+	std::list<T> m_queue; // 任务缓冲区
+	mutable std::mutex m_mutex;
+	std::condition_variable m_notEmpty;	//  C
+	std::condition_variable m_notFull;  //	P
+	int m_maxSize;          // 
+	bool m_needStop;    // true 同步队列不在接受新的任务// 
+
+	bool IsFull() const
+	{
+		bool full = m_queue.size() >= m_maxSize;
+		if (full)
+		{
+			printf("m_queue full....\n");
+		}
+		return full;
+	}
+	bool IsEmpty() const
+	{
+		bool empty = m_queue.empty(); // 
+		if (empty)
+		{
+			printf("m_queue empty....\n");
+		}
+		return empty;
+	}
